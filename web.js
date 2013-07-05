692e2f8e608e8cc6bab9017d1871afcee4daea09
Mariano J. Obarrio Miles <mariano.obarrio@gmail.com>
2013-07-05 15:36:38 +0000

    .

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+
+var indexinfo = fs.readFileSync('./index.html', 'utf8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(indexinfo);
 });
 
 var port = process.env.PORT || 5000;
