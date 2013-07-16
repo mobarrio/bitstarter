7842e8ca299ddfab63cb4bfde6b9659281bf51cb
Mariano J. Obarrio Miles <mariano.obarrio@gmail.com>
2013-07-16 13:41:59 +0000

    Personalizado

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var fs = require('fs');
-
-var indexinfo = fs.readFileSync('./index.html', 'utf8');
-
+var port = process.env.PORT || 8080;
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send(indexinfo);
+app.use('/', express.static(__dirname + "/public"));
+
+app.get('/', function(req, res){
+    fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function() { 
+		console.log("Listening on " + port); 
 });
